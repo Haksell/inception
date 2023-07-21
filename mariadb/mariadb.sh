086432ee588d592b4e62bf4d8f2e916c5ca2668d
haksell <axel.brisse@gmail.com>
2023-07-21 19:19:32 +0200

    simplify mariadb entrypoint script

--- mariadb/mariadb.sh
+++ mariadb/mariadb.sh
@@ -1,68 +1,28 @@
 #!/bin/sh
 
-if [ -d "/run/mysqld" ]; then
-	echo "[i] mysqld already present, skipping creation"
-	chown -R mysql:mysql /run/mysqld
-else
-	echo "[i] mysqld not found, creating...."
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+mkdir -p /run/mysqld
 
 if [ -d /var/lib/mysql/mysql ]; then
-	echo "[i] MySQL directory already present, skipping creation"
-	chown -R mysql:mysql /var/lib/mysql
+	echo "MySQL directory already present, skipping creation"
 else
-	echo "[i] MySQL data directory not found, creating initial DBs"
-	chown -R mysql:mysql /var/lib/mysql
+	echo "MySQL directory not found, creating database '$MYSQL_DATABASE'"
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
-
-	echo "[i] Creating database: $MYSQL_DATABASE"
 	tfile=`mktemp`
-	cat < /scripts/init.sql > $tfile
-	echo "[i] with character set: 'utf8' and collation: 'utf8_general_ci'"
-	echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
-
-	if [ "$MYSQL_USER" != "" ]; then
-		echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-	fi
-
-	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
-	rm -f $tfile
-
-	if [ "$MYSQL_DATABASE" != "" ] && [ "$(ls -A /docker-entrypoint-initdb.d 2>/dev/null)" ]; then
-        echo
-        echo "Preparing to process the contents of /docker-entrypoint-initdb.d/"
-        echo
-		TEMP_OUTPUT_LOG=/tmp/mysqld_output
-		/usr/bin/mysqld --user=mysql --skip-name-resolve --skip-networking=0 --silent-startup > "${TEMP_OUTPUT_LOG}" 2>&1 &
-		PID="$!"
-
-		until tail "${TEMP_OUTPUT_LOG}" | grep -q "Version:"; do
-			sleep 0.2
-		done
-
-		MYSQL_CLIENT="/usr/bin/mysql -u root -p$MYSQL_ROOT_PASSWORD"
-
-		for f in /docker-entrypoint-initdb.d/*; do
-			case "$f" in
-				*.sql)    echo "  $0: running $f"; eval "${MYSQL_CLIENT} ${MYSQL_DATABASE} < $f"; echo ;;
-				*.sql.gz) echo "  $0: running $f"; gunzip -c "$f" | eval "${MYSQL_CLIENT} ${MYSQL_DATABASE}"; echo ;;
-			esac
-		done
-
-    	kill -s TERM "${PID}"
-    	wait "${PID}"
-        rm -f TEMP_OUTPUT_LOG
-    	echo "Completed processing seed files."
-	fi;
-
-	echo
-	echo 'MySQL init process done. Ready for start up.'
-	echo
-
-	echo "exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0" "$@"
+	echo "USE mysql;" >> "$tfile"
+	echo "FLUSH PRIVILEGES ;" >> "$tfile"
+	echo "GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;" >> "$tfile"
+	echo "GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;" >> "$tfile"
+	echo "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;" >> "$tfile"
+	echo "DROP DATABASE IF EXISTS test ;" >> "$tfile"
+	echo "FLUSH PRIVILEGES ;" >> "$tfile"
+	echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> "$tfile"
+	echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$tfile"
+	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$tfile"
+	rm -f "$tfile"
 fi
 
+echo 'MariaDB init process done.'
 exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+echo 'MariaDB exec process done. This is bad.'
