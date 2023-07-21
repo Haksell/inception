bc036f6cae3d823e8def119747d95271c96f4191
haksell <axel.brisse@gmail.com>
2023-07-21 18:21:17 +0200

    init.sql, network name and other stuff

--- mariadb/mariadb.sh
+++ mariadb/mariadb.sh
@@ -2,6 +2,7 @@
 
 for i in /scripts/pre-init.d/*sh
 do
+	echo "script: $i"
 	if [ -e "${i}" ]; then
 		echo "[i] pre-init.d - processing $i"
 		. "${i}"
@@ -22,15 +23,8 @@ if [ -d /var/lib/mysql/mysql ]; then
 	chown -R mysql:mysql /var/lib/mysql
 else
 	echo "[i] MySQL data directory not found, creating initial DBs"
-
 	chown -R mysql:mysql /var/lib/mysql
-
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
-
-	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-		MYSQL_ROOT_PASSWORD=`pwgen 16 1`
-		echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-	fi
 
 	MYSQL_DATABASE=${MYSQL_DATABASE:-""}
 	MYSQL_USER=${MYSQL_USER:-""}
@@ -41,15 +35,7 @@ else
 	    return 1
 	fi
 
-	cat << EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES ;
-GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
-DROP DATABASE IF EXISTS test ;
-FLUSH PRIVILEGES ;
-EOF
+	cat < /scripts/init.sql > $tfile
 
 	if [ "$MYSQL_DATABASE" != "" ]; then
 	    echo "[i] Creating database: $MYSQL_DATABASE"
