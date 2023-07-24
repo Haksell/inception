eb10a8d5069009f14deb24a0b51c09427165a353
haksell <axel.brisse@gmail.com>
2023-07-24 22:31:11 +0200

    setup wordpress

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -3,14 +3,26 @@
 cd /var/www/html
 
 wp core download --allow-root
-wp config create --dbname=mariadb --dbuser=root --dbpass=wordpress --allow-root --skip-check --dbhost=mariadb
+
+rm -f /var/www/html/wp-config.php
+wp config create --dbname=mariadb --dbhost=mariadb --dbuser=root --dbpass=wordpress --allow-root --skip-check
 
 until wp db check --path=/var/www/html --quiet --allow-root; do
     echo "Waiting for MySQL..."
     sleep 1
 done
 
-rm /var/www/html/wp-config.php
-wp core install --url="axbrisse.42.fr" --title="Ooooh very googoo gaga" --admin_user="admin" --admin_password="admin" --admin_email="axbrisse@student.42.fr" --allow-root
+wp core install --url="axbrisse.42.fr" --title="Ooooh very googoo gaga" --admin_user="axbrisse" --admin_password="axbrisse" --admin_email="axbrisse@student.42.fr" --allow-root
+
+# Add a new user with the role of subscriber
+wp user create utenret utenret@example.com --role=subscriber --user_pass=utenret --allow-root
+
+# Change the theme to twenty seventeen
+wp theme install twentyseventeen --activate --allow-root
+
+wp post delete $(wp post list --post_type='post' --field=ID --post_title='Hello World' --allow-root)
+# Create a new blog post
+# TODOcreate only if not exists
+wp post create --post_type=post --post_title="Hello Inception!" --post_content="lol" --post_status=publish --allow-root
 
 php-fpm7.4 -F
