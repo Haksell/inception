8cd7b66a258257e01625de64a6b4cd5ce815ec4b
haksell <axel.brisse@gmail.com>
2023-07-25 01:04:11 +0200

    more env

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -7,7 +7,7 @@ mkdir -p /run/mysqld
 if [ -d /var/lib/mysql/mysql ]; then
 	echo "MySQL directory already present, skipping creation"
 else
-	echo "MySQL directory not found, creating database '$DB_DATABASE'"
+	echo "MySQL directory not found, creating database '$DB_NAME'"
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 	tfile=`mktemp`
 	echo "USE mysql;" >> "$tfile"
@@ -17,8 +17,8 @@ else
 	echo "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${DB_ROOT_PASSWORD}') ;" >> "$tfile"
 	echo "DROP DATABASE IF EXISTS test ;" >> "$tfile"
 	echo "FLUSH PRIVILEGES ;" >> "$tfile"
-	echo "CREATE DATABASE IF NOT EXISTS \`$DB_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> "$tfile"
-	echo "GRANT ALL ON \`$DB_DATABASE\`.* to '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> "$tfile"
+	echo "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> "$tfile"
+	echo "GRANT ALL ON \`$DB_NAME\`.* to '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> "$tfile"
 	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$tfile"
 	rm -f "$tfile"
 fi
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -7,15 +7,15 @@ if [ ! -f $lock_file ]; then
 
     wp core download --allow-root
     rm -f /var/www/html/wp-config.php
-    wp config create --dbname=mariadb --dbhost=mariadb --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD --allow-root --skip-check
+    wp config create --dbname=$DB_NAME --dbhost=$DB_NAME --dbuser=$DB_USERNAME --dbpass=$DB_USER_PASSWORD --allow-root --skip-check
 
     until wp db check --path=/var/www/html --quiet --allow-root; do
         echo "Waiting for MySQL..."
         sleep 1
     done
 
-    wp core install --url="axbrisse.42.fr" --title="Ooooh very googoo gaga" --admin_user="axbrisse" --admin_password="axbrisse" --admin_email="axbrisse@student.42.fr" --allow-root
-    wp user create utenret utenret@example.com --role=subscriber --user_pass=utenret --allow-root
+    wp core install --url="axbrisse.42.fr" --title="Ooooh very googoo gaga" --admin_user=$WP_ADMIN_USERNAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+    wp user create $WP_SUBSCRIBER_USERNAME $WP_SUBSCRIBER_EMAIL --role=subscriber --user_pass=$WP_SUBSCRIBER_PASSWORD --allow-root
     wp theme install twentyseventeen --activate --allow-root
     wp post delete $(wp post list --format=ids --allow-root) --allow-root
     wp post create --post_type=post --post_title="Hello Inception!" --post_content="lol" --post_status=publish --allow-root
