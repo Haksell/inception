abf300b548959909ce127deb60af6e0399c4a2cf
haksell <axel.brisse@gmail.com>
2023-07-25 04:34:27 +0200

    redis Dockerfile

--- srcs/requirements/vsftpd/vsftpd.sh
+++ srcs/requirements/vsftpd/vsftpd.sh
@@ -3,4 +3,7 @@
 useradd -m ftpuser && echo "ftpuser:$FTP_PASSWORD" | chpasswd
 chown -R ftpuser:ftpuser /var/www/html
 mkdir -p /var/run/vsftpd/empty
+
+echo "vsftpd setup finished"
+
 exec /usr/sbin/vsftpd /etc/vsftpd.conf
