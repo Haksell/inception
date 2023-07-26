f97317e610776ad49ed27f0d91d5dd81ceb16ba9
haksell <axel.brisse@gmail.com>
2023-07-26 06:42:07 +0200

    better dots

--- srcs/requirements/bonus/fdf/sketch.js
+++ srcs/requirements/bonus/fdf/sketch.js
@@ -1,47 +1,61 @@
 const COLORS = ['crimson', 'dodgerblue', 'lightblue', 'limegreen']
 const CELL_SIZE = 15
-const CHANGES = 200
+const ACTIVITY = 0.001
 
-let grid = []
-let gridWidth
-let gridHeight
+let grid = [[]]
+let drawAll = false
 
 class Dot {
 	constructor() {
 		this.color = random(COLORS)
 		this.size = random(2, CELL_SIZE)
 	}
+
+	draw(x, y) {
+		fill('black')
+		square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE)
+		fill(grid[y][x].color)
+		circle((x + 0.5) * CELL_SIZE, (y + 0.5) * CELL_SIZE, grid[y][x].size)
+	}
+}
+
+const resizeGrid = () => {
+	const newGridHeight = Math.max(1, Math.ceil(windowHeight / CELL_SIZE))
+	const newGridWidth = Math.max(1, Math.ceil(windowWidth / CELL_SIZE))
+	grid = Array.from({ length: newGridHeight }, (_, y) =>
+		Array.from({ length: newGridWidth }, (_, x) =>
+			y < grid.length && x < grid[y].length ? grid[y][x] : new Dot()
+		)
+	)
+	resizeCanvas(windowWidth, windowHeight)
+	drawAll = true
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight)
 	noStroke()
-
-	gridWidth = windowWidth / CELL_SIZE
-	gridHeight = windowHeight / CELL_SIZE
-	for (let i = 0; i < gridWidth; i++) {
-		row = []
-		for (let j = 0; j < gridHeight; j++) {
-			row.push(new Dot())
-		}
-		grid.push(row)
-	}
+	resizeGrid()
 }
 
 function draw() {
-	background('black')
-	for (let i = 0; i < width / CELL_SIZE; i++) {
-		for (let j = 0; j < height / CELL_SIZE; j++) {
-			const dot = grid[i][j]
-			fill(grid[i][j].color)
-			circle(
-				(i + 0.5) * CELL_SIZE,
-				(j + 0.5) * CELL_SIZE,
-				grid[i][j].size
-			)
+	if (drawAll) {
+		for (let y = 0; y < grid.length; y++) {
+			for (let x = 0; x < grid[y].length; x++) {
+				grid[y][x].draw(x, y)
+			}
 		}
+		drawAll = false
 	}
-	for (let i = 0; i < CHANGES; i++) {
-		grid[int(random(grid.length))][int(random(grid[0].length))] = new Dot()
+	const changes = int(deltaTime * grid.length * grid[0].length * ACTIVITY)
+	for (let i = 0; i < changes; ++i) {
+		const y = int(random(grid.length))
+		const x = int(random(grid[y].length))
+		grid[y][x] = new Dot()
+		grid[y][x].draw(x, y)
 	}
+}
+
+function windowResized() {
+	resizeGrid()
+	resizeCanvas(windowWidth, windowHeight)
 }
