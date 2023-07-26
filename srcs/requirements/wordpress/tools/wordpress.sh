0bb2e6a22f514da03f5fd16acda36f559d8206fd
haksell <axel.brisse@gmail.com>
2023-07-26 07:22:40 +0200

    fdf -> dots + wordpress favicon

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -15,13 +15,17 @@ if [ ! -f $lock_file ]; then
     done
 
     wp core install --url="axbrisse.42.fr" --title="Ooooh very googoo gaga" --admin_user=$WP_ADMIN_USERNAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+    
+    wp config set WP_REDIS_HOST redis --allow-root
+  	wp config set WP_REDIS_PORT 6379 --raw --allow-root
+  	wp config set ALLOW_UNFILTERED_UPLOADS true --raw --allow-root
+
     wp user create $WP_SUBSCRIBER_USERNAME $WP_SUBSCRIBER_EMAIL --role=subscriber --user_pass=$WP_SUBSCRIBER_PASSWORD --allow-root
     wp theme install twentyseventeen --activate --allow-root
     wp post delete $(wp post list --format=ids --allow-root) --allow-root
     wp post create --post_type=post --post_title="Hello Inception!" --post_content="lol" --post_status=publish --allow-root
+    wp media import /42logo.png --porcelain --allow-root | wp option update site_icon --allow-root
 
-    wp config set WP_REDIS_HOST redis --allow-root
-  	wp config set WP_REDIS_PORT 6379 --raw --allow-root
     wp plugin install redis-cache --activate --allow-root
     wp redis enable --force --allow-root --path=/var/www/html
 
