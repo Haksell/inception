a93207970329242ffcbbfdadc586a3e6f803e5d9
haksell <axel.brisse@gmail.com>
2023-07-26 06:00:49 +0200

    better dots

--- srcs/requirements/bonus/fdf/sketch.js
+++ srcs/requirements/bonus/fdf/sketch.js
@@ -22,9 +22,7 @@ function setup() {
 	for (let i = 0; i < gridWidth; i++) {
 		row = []
 		for (let j = 0; j < gridHeight; j++) {
-			let rand_color = random(COLORS)
-			let rand_size = random(2, CELL_SIZE)
-			row.push([rand_color, rand_size])
+			row.push(new Dot())
 		}
 		grid.push(row)
 	}
@@ -34,16 +32,16 @@ function draw() {
 	background('black')
 	for (let i = 0; i < width / CELL_SIZE; i++) {
 		for (let j = 0; j < height / CELL_SIZE; j++) {
-			fill(grid[i][j][0])
-			circle((i + 0.5) * CELL_SIZE, (j + 0.5) * CELL_SIZE, grid[i][j][1])
+			const dot = grid[i][j]
+			fill(grid[i][j].color)
+			circle(
+				(i + 0.5) * CELL_SIZE,
+				(j + 0.5) * CELL_SIZE,
+				grid[i][j].size
+			)
 		}
 	}
 	for (let i = 0; i < CHANGES; i++) {
-		let rand_color = random(COLORS)
-		let rand_size = random(2, CELL_SIZE)
-		grid[int(random(grid.length))][int(random(grid[0].length))] = [
-			rand_color,
-			rand_size,
-		]
+		grid[int(random(grid.length))][int(random(grid[0].length))] = new Dot()
 	}
 }
