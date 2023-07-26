65a0525d282138e64afb9610b663f7f0105d76b8
haksell <axel.brisse@gmail.com>
2023-07-26 08:01:37 +0200

    add MARIADB_

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -7,18 +7,18 @@ mkdir -p /run/mysqld
 if [ -d /var/lib/mysql/mysql ]; then
 	echo "MySQL directory already present, skipping creation"
 else
-	echo "MySQL directory not found, creating database '$DB_NAME'"
+	echo "MySQL directory not found, creating database '$MARIADB_NAME'"
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 	tfile=`mktemp`
 	echo "USE mysql;" >> "$tfile"
 	echo "FLUSH PRIVILEGES ;" >> "$tfile"
-	echo "GRANT ALL ON *.* TO 'root'@'%' identified by '$DB_ROOT_PASSWORD' WITH GRANT OPTION ;" >> "$tfile"
-	echo "GRANT ALL ON *.* TO 'root'@'localhost' identified by '$DB_ROOT_PASSWORD' WITH GRANT OPTION ;" >> "$tfile"
-	echo "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${DB_ROOT_PASSWORD}') ;" >> "$tfile"
+	echo "GRANT ALL ON *.* TO 'root'@'%' identified by '$MARIADB_ROOT_PASSWORD' WITH GRANT OPTION ;" >> "$tfile"
+	echo "GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MARIADB_ROOT_PASSWORD' WITH GRANT OPTION ;" >> "$tfile"
+	echo "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MARIADB_ROOT_PASSWORD}') ;" >> "$tfile"
 	echo "DROP DATABASE IF EXISTS test ;" >> "$tfile"
 	echo "FLUSH PRIVILEGES ;" >> "$tfile"
-	echo "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> "$tfile"
-	echo "GRANT ALL ON \`$DB_NAME\`.* to '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';" >> "$tfile"
+	echo "CREATE DATABASE IF NOT EXISTS \`$MARIADB_NAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> "$tfile"
+	echo "GRANT ALL ON \`$MARIADB_NAME\`.* to '$MARIADB_USERNAME'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';" >> "$tfile"
 	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$tfile"
 	rm -f "$tfile"
 fi
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -7,7 +7,7 @@ if [ ! -f $lock_file ]; then
 
     wp core download --allow-root
     rm -f /var/www/html/wp-config.php
-    wp config create --dbname=$DB_NAME --dbhost=$DB_NAME --dbuser=$DB_USERNAME --dbpass=$DB_USER_PASSWORD --allow-root --skip-check
+    wp config create --dbname=$MARIADB_NAME --dbhost=$MARIADB_NAME --dbuser=$MARIADB_USERNAME --dbpass=$MARIADB_USER_PASSWORD --allow-root --skip-check
 
     until wp db check --path=/var/www/html --quiet --allow-root; do
         echo "Waiting for MySQL..."
